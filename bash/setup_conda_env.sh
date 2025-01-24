c4a7be6fb39d6d203f1ec35a316fc64ac381dc5f
PhatLeBao-Phat <104715759+PhatLeBao-Phat@users.noreply.github.com>
2025-01-24 14:25:26 +0100

    change to pyenv from conda

--- bash/setup_conda_env.sh
+++ bash/setup_conda_env.sh
@@ -3,10 +3,12 @@
 # Check if the environment exists
 if conda env list | grep -q "env"; then
     echo "Environment exists. Updating..."
-    conda env update --name env --file /app/environment.yml --prune
+    conda env update --name env --file environment.yml --prune 
+    echo "Environment Updated"
 else
     echo "Environment does not exist. Creating..."
-    conda env create --name env --file /app/environment.yml
+    conda env create --name env --file environment.yml
+    echo "Environment created"
 fi
 
 # Activate the environment and start an interactive bash session
